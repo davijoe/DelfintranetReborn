3c3dd1bb839b939881512e7909e10894c31c3e50
Dan Jørgensen <61663729+davijoe@users.noreply.github.com>
2023-01-31 17:43:06 +0100

    medlem

---
+++ src/Delfin.java
@@ -0,0 +1,5 @@
+public class Delfin{
+
+   Medlem medlem1 = new Medlem("Dan",30,true);
+   System.out.println(medlem1.name);
+}
---
+++ src/Medlem.java
@@ -0,0 +1,47 @@
+public class Medlem{
+
+   String name;
+   int alder;
+   boolean medlemsaktivitet;
+   int medlemsId;
+      
+      public medlem(String n, int a, boolean ma){
+         name = n;
+         alder = a;
+         medlemsaktivitet = ma;
+         medlemsId = id;
+      }
+      
+      public String getName(){
+         return name;
+      }
+      
+      Public void setName(){
+         this.name = newName;
+      }
+      
+      public String getAlder(){
+         return alder;
+      }
+      
+      Public void setName(){
+         this.alder = newAlder;
+      }
+      public String getMedlemsaktivitet(){
+         return medlemsaktivitet;
+      }
+      
+      Public void setMedlemsaktivitet(){
+         this.medlemsaktivitet = newMedlemsaktivitet;
+      }
+      public String getMedlemsId(){
+         return medlemsId;
+      }
+      
+      Public void setMedlemsId(){
+         this.medlemsId = newMedlemsId;
+      }
+                        
+      
+}
+   
